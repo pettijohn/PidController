2d4408bb85c7b2586e18528bd54d8d0fa0853488
pettijohn <hello@world.com>
2013-09-18 16:31:20 +1000

    Draft of DI and Scheduler thinking

---
+++ PidController/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace PidController
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var diFramework = new DIFramework();
+            diFramework.Register<ISensorState>().To<SensorState>().AsSingleton();
+            diFramework.Register<IThermocouple>().To<Thermocouple>().AsSingleton();
+
+            var scheduler = new TaskSchedulerEngine();
+            scheduler.ResolveReference = (t) =>
+            {
+                return diFramework.Resolve(t);
+            };
+
+            scheduler.Add(new Schedule("everySecond").EverySecond().Execute<IThermocouple>);
+
+            scheduler.Start();
+            scheduler.Wait();
+        }
+
+    }
+}
